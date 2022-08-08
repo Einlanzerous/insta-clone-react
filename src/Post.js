7b01635c9fd8260314f3f33b67a9203ca4a9b86d
einlanzerous <einlanzerous@live.com>
2022-08-07 23:37:56 -0400

    Added post headers and user details. Delete button in progress

--- src/Post.js
+++ src/Post.js
@@ -1,26 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import './Post.css';
+import React, { useState, useEffect } from "react";
+import "./Post.css";
+import { Avatar, Button } from "@mui/material"
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = "http://localhost:8000";
 
 function Post({ post }) {
-
-  const [imageUrl, setImageUrl] = useState('');
+  const [imageUrl, setImageUrl] = useState("");
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    if (post.image_url_type === 'absolute') {
-      setImageUrl(post.image_url)
+    if (post.image_url_type === "absolute") {
+      setImageUrl(post.image_url);
     } else {
       setImageUrl(`${BASE_URL}/${post.image_url}`);
     }
-    }, []);
+  }, []);
 
+  useEffect(() => {
+    setComments(post.comments);
+  }, []);
 
   return (
-    <div className='post'>
-      <img className='post_img' src={imageUrl} alt='post' />
+    <div className="post">
+      <div className="post_header">
+        <Avatar alt="post.user.username" src="/" />
+        <div className="post_header_info">
+          <h3>{post.user.username}</h3>
+          <Button className="post_delete">Delete</Button>
+        </div>
+      </div>
+
+      <img className="post_img" src={imageUrl} alt="post" />
+
+      <h4 className="post_message">{post.caption}</h4>
+
+      <div className="post_comments">
+        {comments.map((comment) => (
+          <p>
+            <strong>{comment.username}:</strong> {comment.message}
+          </p>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export { Post };
